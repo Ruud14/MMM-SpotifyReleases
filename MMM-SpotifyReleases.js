2c5e854e3d25835f73242535c652e42df3df598f
Ruud14 <ruudbrouwers@hotmail.com>
2020-08-29 16:32:33 +0200

    Show more info

--- MMM-SpotifyReleases.js
+++ MMM-SpotifyReleases.js
@@ -6,7 +6,7 @@ Module.register("MMM-SpotifyReleases", {
 		state: "NY",
         useHeader: true, // false if you don't want a header
         header: "New releases of your favorite artists!", // Any text you want
-        maxWidth: "250px",
+        maxWidth: "400px",
         rotateInterval: 30 * 1000,
         animationSpeed: 3000, // fade in and out speed
         initialLoadDelay: 4250,
@@ -29,6 +29,7 @@ Module.register("MMM-SpotifyReleases", {
         this.accessToken = "";
         this.market = "NL";
         this.maxAlbumsPerRequest = "5";
+        this.visibleReleases = 5;
         this.url = "https://api.spotify.com/v1/artists/" + "7AGSJihqYPhYy5QzMcwcQT"+ "/albums?market=" + this.market + "&limit=" + this.maxAlbumsPerRequest;
         this.url_start = "https://api.spotify.com/v1/artists/";
         this.url_end = "/albums?market=" + this.market + "&limit=" + this.maxAlbumsPerRequest;
@@ -37,16 +38,19 @@ Module.register("MMM-SpotifyReleases", {
 
     getDom: function() {
 
+        // Setup the wrapper.
         var wrapper = document.createElement("div");
         wrapper.className = "wrapper";
         wrapper.style.maxWidth = this.config.maxWidth;
 
+        // Show loading text when the spotify api call hasn't been completed yet.
         if (!this.loaded) {
             wrapper.innerHTML = "Loading new releases of your favorite artists...";
             wrapper.classList.add("bright", "light", "small");
             return wrapper;
         }
 
+        // Show the header.
         if (this.config.useHeader != false) {
             var header = document.createElement("header");
             header.classList.add("xsmall", "bright", "light", "header");
@@ -57,25 +61,65 @@ Module.register("MMM-SpotifyReleases", {
         var top = document.createElement("div");
         top.classList.add("list-row");
         
-        console.log(this.allAlbums);
+        // Sort all albums by releasedate.
+        var sortedAlbumList = Object.values(this.allAlbums);
+        sortedAlbumList = sortedAlbumList.sort((a, b) => (parseInt(b.release_date.split("-").join("")) - parseInt(a.release_date.split("-").join(""))))
 
-        for(var album_uri in this.allAlbums)
+        // Show the most recent albums.
+        for(const album_i in sortedAlbumList.slice(0, this.visibleReleases))
         {
-            var album = this.allAlbums[album_uri];
-            var name = album.name;
-            var albumType = album.albumType;
-            var releaseDate = album.release_date;
-            var totalTracks = album.totalTracks;
-            var uri = album.uri;
-            var duration = document.createElement("div");
-            duration.classList.add("xsmall", "bright", "duration");
-            duration.innerHTML = "" + name;
-            wrapper.appendChild(duration);
+            var album = sortedAlbumList[album_i];
+
+            var div = document.createElement("div");
+            div.classList.add("xsmall", "bright", "duration", "AlbumCard");
+
+            // Show the release date, title and album/single as a horizontal list.
+            var top_list = document.createElement("ul");
+            top_list.classList.add("horizontalList");
+            // Release date.
+            var releaseDate_elem = document.createElement("h10");
+            // Calculate the day difference.
+            var releaseDate = new Date(album.release_date);
+            var today = new Date();
+            const diffTime = Math.abs(today - releaseDate);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+        
+            releaseDate_elem.innerText = diffDays.toString() + " days ago";
+            top_list.appendChild(releaseDate_elem);
+            // Album type.
+            var albumType_elem = document.createElement("h10");
+            albumType_elem.classList.add("AlbumType");
+            albumType_elem.innerText = album.album_type;
+            top_list.appendChild(albumType_elem);
+            // Title.
+            var title_elem = document.createElement("h3");
+            title_elem.classList.add("AlbumName");
+            title_elem.innerText = album.name;
+            top_list.appendChild(title_elem);
+
+            div.appendChild(top_list);
+
+            // Show the artists to a horizontal list.
+            var artists_elem = document.createElement("ul");
+            artists_elem.classList.add("horizontalList");
+            for(const artist_i in album.artists)
+            {
+                var artist = album.artists[artist_i];
+                var artist_name = artist.name;
+                var artist_elem = document.createElement("li");
+                artist_elem.classList.add("Artist");
+                artist_elem.innerText = artist_name;
+                artists_elem.appendChild(artist_elem);
+            }
+            div.appendChild(artists_elem);
+
+            wrapper.appendChild(div);
         }
         return wrapper;
 		
     },
-	
+    
+    // Schedules the refreshing of the recent releases.
     scheduleUpdate: function() { 
         setInterval(() => {
             this.getRecentAlbums();
@@ -84,6 +128,7 @@ Module.register("MMM-SpotifyReleases", {
         var self = this;
     },
 
+    // Get the most recent albums of the specified artist.
     getAlbumsOfArtist: function(artistID)
     {
         var url = this.url_start + artistID + this.url_end;
@@ -105,6 +150,7 @@ Module.register("MMM-SpotifyReleases", {
         req.send();
     },
 
+    // Get all the recent albums.
     getRecentAlbums : function()
     {
         for(const artist_i in this.artists)
